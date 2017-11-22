6812ddc3c3adf939037480220214cfc263d88850
bharath sivaraman <bharathsivaram@gmail.com>
2017-11-22 10:12:10 -0500

    Baseline

--- HousePrices/HousePrices.R
+++ HousePrices/HousePrices.R
@@ -37,6 +37,12 @@ test <- read.csv("test.csv")
 test$SalePrice <- 1
 combined <- rbind(train, test)
 
+# Function------------
+
+impute.median <-
+  function(x)
+    replace(x, is.na(x), median(x, na.rm = TRUE))
+
 
 # EDA - Feature Engineering --------------------------------------------------
 
@@ -56,39 +62,125 @@ for (i in 1:ncol(combined)) {
 }
 colnames <- colnames[-1]
 
+##Remove columns with more than 40%NA
 combined <- combined %>% select(-one_of(colnames))
 
+#convert into factors
+combined$YearBuilt <- as.factor(combined$YearBuilt)
+combined$YearRemodAdd <- as.factor(combined$YearRemodAdd)
+combined$GarageYrBlt <- as.factor(combined$GarageYrBlt)
+combined$YrSold <- as.factor(combined$YrSold)
+combined$MSSubClass <- as.factor(combined$MSSubClass)
+
+#get factor columns
+factor.variables <- combined[, lapply(combined, is.factor) == TRUE]
+
+factor.variables$SalePrice <- combined$SalePrice
+
+##-------- Check freq and dis of every level in each column
+data.levels <- data.frame(
+  colname = "x",
+  levels = 1,
+  Var1 = "x",
+  Freq = 1,
+  AvgSalePrice = 1
+)
+
+data.levels.tmp <- data.levels
 
 
 
+for (i in 1:(ncol(factor.variables) - 1))
+{
+  factor.variables[, i] <- as.character(factor.variables[, i])
+  factor.variables[which(is.na(factor.variables[, i]) == TRUE), i] <-
+    "Missing"
+  factor.variables[, i] <-
+    as.factor(as.character(factor.variables[, i]))
+  
+  colname.group <- colnames(factor.variables)[i]
+  data.levels.tmp <-
+    data.frame(
+      colname = colnames(factor.variables[i]),
+      levels = length(unlist(levels(
+        factor.variables[[i]]
+      ))),
+      as.data.frame(table(factor.variables[i]))
+    )
+  
+  
+  x <-
+    factor.variables %>% select(i, ncol(factor.variables)) %>% group_by_(colname.group) %>% summarize(AvgSalePrice =
+                                                                                                        mean(SalePrice)) %>%
+    rename_(Var1 = colname.group)
+  data.levels.tmp <- data.levels.tmp %>% inner_join(x, "Var1")
+  
+  data.levels <- rbind(data.levels, data.levels.tmp)
+}
+
+rm(data.levels.tmp)
+rm(x)
+data.levels <- data.levels[-1, ]
+data.levels$Dist <-
+  (data.levels$Freq / nrow(factor.variables)) * 100
+
+colname.remove <-
+  data.levels %>% filter(Dist > 95) %>% select(colname)
+
+colname.remove <- as.vector(colname.remove$colname)
+factor.variables <-
+  factor.variables %>% select(-one_of(colname.remove))
+
+data.levels <-
+  data.levels %>% group_by(colname) %>% mutate(rnk = min_rank((Dist))) %>% arrange(colname, rnk)
+data.levels <-
+  data.levels %>% mutate(cumDist = cumsum(Dist)) %>% ungroup()
+
+data.levels <-
+  data.levels %>% mutate(newlevel = ifelse(cumDist < 5, "Other", as.character(Var1)))
+
+####----- Merge levels where distribution is less than 5%
+
+new <- factor.variables
+new[] <-
+  lapply(factor.variables, function(x)
+    data.levels$newlevel[match(x, data.levels$Var1)])
+factor.variables <- new[-38]
+factor.variables <- lapply(factor.variables, as.factor)
+factor.variables <- as.data.frame(factor.variables)
+
+##-----------Check for NA's in continuous variables and impute
+continuous.variables <-
+  combined[, lapply(combined, is.numeric) == TRUE]
+continuous.variables <- continuous.variables[,-1]
+
+for (i in 1:ncol(continuous.variables))
+{
+  continuous.variables[[i]] <-
+    impute.median(continuous.variables[[i]])
+}
+
+
+combined.clean <- cbind(factor.variables, continuous.variables)
+combined.clean$Id <- combined$Id
+combined.NA <- combined.clean[!complete.cases(combined.clean), ]
+summary(combined.clean)
+
+
+train.predict <- combined.clean %>% filter(Id %in% train[["Id"]])
+test.predict <- combined.clean %>% filter(Id %in% test[["Id"]])
 
 ##SalePrice is right skewed and taking log removes the skewness
-ggplot(train) + aes(SalePrice) + geom_histogram(col = "white") + theme_light()
-ggplot(train) + aes(log(SalePrice)) + geom_histogram(col = "white", fill =
-                                                       "firebrick") + theme_light()
-train$MSSubClass <- as.factor(train$MSSubClass)
+ggplot(train.predict) + aes(SalePrice) + geom_histogram(col = "white") + theme_light()
+ggplot(train.predict) + aes(log(SalePrice)) + geom_histogram(col = "white", fill = "firebrick") + theme_light()
 
-continuous.variable <- train[, lapply(train, is.numeric) == TRUE]
-continuous.variable <- continuous.variable[-1]
-
-factor.variables <- train[, lapply(train, is.factor) == TRUE]
-factor.variables$SalePrice <- train$SalePrice
-factor.variables$SalePrice <- train$SalePrice
-##Neighbourhood
-ggplot(factor.variables) + aes(Neighborhood, SalePrice) + geom_boxplot()
+train.predict$SalePrice <- log(train.predict$SalePrice)
 
 
-#Baseline Model----
+# Baseline Model--------------------------------------------------
 ##Baseline model built on averaging the price across neighbourhood
 
-impute.mean <-
-  function(x)
-    replace(x, is.na(x), mean(x, na.rm = TRUE))
-
-#continuous.variable$YearBuilt<-as.factor(continuous.variable$YearBuilt)
-continuous.variable <-
-  continuous.variable %>% group_by(YearBuilt) %>% mutate(SalePrice =
-                                                           impute.mean(SalePrice)) %>% ungroup()
+train.predict <- train.predict %>% select(-one_of("Id"))
 
 predictors <- SalePrice ~ .
 
@@ -96,18 +188,20 @@ trcntrl <- trainControl(
   method = "repeatedcv",
   number = 10,
   repeats = 3,
-  summaryFunction = twoClassSummary,
-  classProbs = TRUE,
+  
   savePredictions = TRUE
 )
 
+set.seed(1234)
 baselineLM <- train(
   predictors,
-  A =
-    data = continuous.variable,
+  
+  data = train.predict,
   method = "lm",
   trControl = trcntrl,
-  tuneLength = 5 ,
-  metric = "Accuracy",
+  tuneLength = 5,
   preProcess = c("center", "scale")
 )
+
+baseline <- predict(baselineLM, test.predict)
+
